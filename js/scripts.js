ad9af8c8e84fbbf578525a442836e8cf95c74f31
aghamu <112896167+aghamu@users.noreply.github.com>
2022-10-08 18:48:02 -0400

    standardize and fix js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-console.log("Welcome to my Website!");
+console.log('Welcome to my Website!')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
+
+menuToggle()
